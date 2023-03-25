591c3bba8175a64c3976c7530b873deff00ecf19
Tze Yeen Liew <tzeyeenliew@gmail.com>
2023-03-25 09:13:46 +0100

    Create SQL Advanced Queries.sql

---
+++ SQL Advanced Queries.sql
@@ -0,0 +1,20 @@
+SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''));
+USE sakila;
+
+### Q2 For each film, list actor that has acted in more films.
+
+SELECT F.title,FA.actor_id, CONCAT(b.first_name,' ', b.last_name) AS Name
+FROM film AS F
+JOIN film_actor FA
+USING (film_id)
+JOIN actor AS B
+USING (actor_id)
+GROUP BY film_id
+ORDER BY FA.actor_id IN (SELECT * FROM
+                        (SELECT actor_id 
+						FROM film_actor 
+                         GROUP BY actor_id 
+                         ORDER BY count(film_id) 
+                         DESC 
+                         LIMIT 1 ) 
+                         AS subtable);
